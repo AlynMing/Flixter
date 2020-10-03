cbbaf1d58a9b16e8f50e73279af34790a27b3230
beverleyokogwu <62040799+beverleyokogwu@users.noreply.github.com>
2020-10-02 22:07:51 -0400

    Created DetailActivity.java

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -84,6 +86,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         //define member variables from the item_movie.xml file to
         //bind data
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -96,6 +99,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
         }
 
@@ -103,7 +107,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
          * populate views with getter methods
          * @param movie
          */
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -119,10 +123,25 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
                 imageUrl= movie.getPosterPath();
             }
 
-
-
             //render an image using the Glide library
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            //responds when the title is clicked.
+            /*
+            We want the click listener to respond whenever the
+            viewholder is clicked. Steps outlined below:
+             */
+            //Step 1. Register Click listener on the whole row
+            //             will need to get a reference to the container
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // 2. Navigate to a new activity on tap.
+                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
 
         }
     }
